c6be216d5f45b29cd9bee3472a4fc1cd5e0390fd
salehbenchikh <salehdz999ful2@gmail.com>
2024-11-13 17:43:05 +0100

    Your commit message describing the changes

--- src/components/ProductDetailsPage.tsx
+++ src/components/ProductDetailsPage.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../store/cartSlice";
 import { useParams } from "next/navigation";
-import Image from "next/image";
 
 interface Product {
   _id: string;
@@ -70,33 +69,25 @@ const ProductDetailsPage = () => {
       <div className="w-full lg:flex gap-4 justify-between items-center mx-auto">
         <div className="w-full lg:w-[1000px]">
           <div className="flex space-x-2 mb-4 justify-center">
-          import Image from "next/image";
-
-// تعديل داخل دالة render
-{product.images.map((image, index) => (
-  <Image
-    key={index}
-    src={image}
-    alt={`Thumbnail ${index + 1}`}
-    width={64} // العرض المطلوب للصورة المصغرة
-    height={64} // الارتفاع المطلوب للصورة المصغرة
-    className={`w-16 h-16 object-cover rounded cursor-pointer ${
-      index === currentImageIndex ? "border-2 border-blue-500" : "border"
-    }`}
-    onClick={() => handleThumbnailClick(index)}
-  />
-))}
-
-<div className="flex justify-center items-center bg-gray-200 overflow-hidden rounded-lg md:h-[500px] p-5">
-  <Image
-    src={product.images[currentImageIndex]}
-    alt={`Image ${currentImageIndex + 1} of ${product.name}`}
-    width={500} // العرض المطلوب للصورة الكبيرة
-    height={500} // الارتفاع المطلوب للصورة الكبيرة
-    className="object-contain h-full w-full"
-  />
-</div>
-
+            {product.images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+                className={`w-16 h-16 object-cover rounded cursor-pointer ${
+                  index === currentImageIndex ? "border-2 border-blue-500" : "border"
+                }`}
+                onClick={() => handleThumbnailClick(index)}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center items-center bg-gray-200 overflow-hidden rounded-lg md:h-[500px] p-5">
+            <img
+              src={product.images[currentImageIndex]}
+              alt={`Image ${currentImageIndex + 1} of ${product.name}`}
+              className="object-contain h-full w-full"
+            />
+          </div>
         </div>
         <div className="w-full flex flex-col mt-6 lg:mt-0 gap-y-2 ">
           <h2 className="text-2xl font-bold">{product.name}</h2>
