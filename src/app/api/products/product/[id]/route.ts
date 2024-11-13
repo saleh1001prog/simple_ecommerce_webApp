6a29be640c832f6ee2c3d3bdcc05a3d47973c5ba
salehbenchikh <salehdz999ful2@gmail.com>
2024-11-13 19:01:16 +0100

    Your commit message describing

--- src/app/api/products/product/[id]/route.ts
+++ src/app/api/products/product/[id]/route.ts
@@ -1,8 +1,8 @@
 import dbConnect from '@/lib/mongodb';
 import Product from '@/models/Product'; // Assuming the model is saved in src/models/Product
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET( { params }: { params: { id: string } }) {
   // يتم استخراج `id` مباشرة من `params`
   const { id } = params;
 
